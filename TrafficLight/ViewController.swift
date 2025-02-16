2991cbeda20269ed0fb6e1d8829a2dcab127afaa
Aleks Silber <a.silber@yandex.ru>
2025-02-16 14:03:31 +1000

    finish

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -1,19 +1,56 @@
-//
-//  ViewController.swift
-//  TrafficLight
-//
-//  Created by Alex Silber on 23.08.2024.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+private  enum CurrentLight {
+    case red, yellow, green
+}
 
+final class ViewController: UIViewController {
+
+    @IBOutlet private var redLight: UIView!
+    @IBOutlet private var yellowLight: UIView!
+    @IBOutlet private var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
+    
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentLight = .red
+        }
+    }
 }
 
